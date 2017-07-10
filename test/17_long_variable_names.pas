4ab4b537afc32feb0cafe6e54f8eeeac1c4f2759
Ilya Gulkov <isgulkov@edu.hse.ru>
2017-07-10 21:55:54 +0300

    Add test for long variable names

---
+++ test/17_long_variable_names.pas
@@ -0,0 +1,12 @@
+var
+b9IUxW5WgFmD2ADagyrcozicJLQYC1lpvA7rHPFO : double;
+LM7LGPgEVJTaSwJOhuCSWq2G2QkyqNgzY9yi3K9G : boolean;
+begin
+	b9IUxW5WgFmD2ADagyrcozicJLQYC1lpvA7rHPFO := 13.37;
+	writeln('b9IUxW5WgFmD2ADagyrcozicJLQYC1lpvA7rHPFO equals...');
+	writeln(b9IUxW5WgFmD2ADagyrcozicJLQYC1lpvA7rHPFO);
+
+	LM7LGPgEVJTaSwJOhuCSWq2G2QkyqNgzY9yi3K9G := True;
+	writeln('LM7LGPgEVJTaSwJOhuCSWq2G2QkyqNgzY9yi3K9G equals...');
+	writeln(LM7LGPgEVJTaSwJOhuCSWq2G2QkyqNgzY9yi3K9G);
+end.
