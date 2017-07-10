bdd7ed93c3829091e4dcaf725c361ebbfc753111
Ilya Gulkov <isgulkov@edu.hse.ru>
2017-07-10 21:35:28 +0300

    Add test for boolean operator precedence

---
+++ test/07_bool_operator_precedence.pas
@@ -0,0 +1,16 @@
+
+var
+a, b : boolean;
+begin
+	
+
+	a := not True and False;
+	writeln('not True and False equals...');
+	writeln(a);
+
+	b := not (True and False);
+	writeln('not (True and False) equals...');
+	writeln(b);
+
+end.
+
