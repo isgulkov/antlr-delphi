1d0df0ddc95375ef40f0b7ac83932b62af76fcb3
Ilya Gulkov <isgulkov@edu.hse.ru>
2017-07-10 21:39:57 +0300

    Add test for double expressions

---
+++ test/09_double_expressions.pas
@@ -0,0 +1,19 @@
+
+var
+a, b : double;
+begin
+	
+
+	a := 10.0 - 5.0;
+	writeln('10.0 - 5.0 equals...');
+	writeln(a);
+
+	b := 1.0 / 0.5 + 100.0001;
+	writeln('1.0 / 0.5 + 100.0001 equals...');
+	writeln(b);
+
+	writeln('(1.0 - 1.0 + 1.0) / 2.0 equals...');
+	writeln((1.0 - 1.0 + 1.0) / 2.0);
+
+end.
+
