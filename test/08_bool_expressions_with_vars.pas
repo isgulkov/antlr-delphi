f027b04a6b539c936fc819441edbc4f2735e4934
Ilya Gulkov <isgulkov@edu.hse.ru>
2017-07-10 21:38:09 +0300

    Add test for boolean expressions with variables

---
+++ test/08_bool_expressions_with_vars.pas
@@ -0,0 +1,28 @@
+
+var
+a, b : boolean;
+begin
+	
+
+	a := True;
+	writeln('a equals...');
+	writeln(a);
+
+	b := False;
+	writeln('b equals...');
+	writeln(b);
+
+	writeln('a and b equals...');
+	writeln(a and b);
+
+	writeln('a or b equals...');
+	writeln(a or b);
+
+	writeln('not a equals...');
+	writeln(not a);
+
+	writeln('a and a equals...');
+	writeln(a and a);
+
+end.
+
