000ea61882355cfc084149cfbbbec5a2d1272f46
Ilya Gulkov <isgulkov@edu.hse.ru>
2017-07-10 21:43:17 +0300

    Add test for double expressions with variables

---
+++ test/11_double_expressions_with_vars.pas
@@ -0,0 +1,16 @@
+var
+x, y : double;
+begin
+	x := 13;
+	writeln('x equals...');
+	writeln(x);
+
+	y := 10 * 3 + 8;
+	writeln('y equals...');
+	writeln(y);
+
+	writeln('100 * x + y equals...');
+	writeln(100 * x + y);
+
+end.
+
