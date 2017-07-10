25f782742c12d964982962c2f958a3954bd287a9
Ilya Gulkov <isgulkov@edu.hse.ru>
2017-07-10 22:03:30 +0300

    Add test.bat script

---
+++ test.bat
@@ -0,0 +1,70 @@
+md \test\compiled
+
+res\DelphiTranslator.exe test\01_assignment_provided.pas test\compiled\01_assignment_provided.cs
+"C:\Program Files\MSBuild\14.0\Bin\csc.exe" \test\compiled\01_assignment_provided.cs /out:test\compiled\01_assignment_provided.exe
+\test\compiled\01_assignment_provided.exe
+
+res\DelphiTranslator.exe test\02_multiple_declarations.pas test\compiled\02_multiple_declarations.cs
+"C:\Program Files\MSBuild\14.0\Bin\csc.exe" \test\compiled\02_multiple_declarations.cs /out:test\compiled\02_multiple_declarations.exe
+\test\compiled\02_multiple_declarations.exe
+
+res\DelphiTranslator.exe test\03_multiple_assignments.pas test\compiled\03_multiple_assignments.cs
+"C:\Program Files\MSBuild\14.0\Bin\csc.exe" \test\compiled\03_multiple_assignments.cs /out:test\compiled\03_multiple_assignments.exe
+\test\compiled\03_multiple_assignments.exe
+
+res\DelphiTranslator.exe test\04_assignments_and_prints.pas test\compiled\04_assignments_and_prints.cs
+"C:\Program Files\MSBuild\14.0\Bin\csc.exe" \test\compiled\04_assignments_and_prints.cs /out:test\compiled\04_assignments_and_prints.exe
+\test\compiled\04_assignments_and_prints.exe
+
+res\DelphiTranslator.exe test\05_string_prints.pas test\compiled\05_string_prints.cs
+"C:\Program Files\MSBuild\14.0\Bin\csc.exe" \test\compiled\05_string_prints.cs /out:test\compiled\05_string_prints.exe
+\test\compiled\05_string_prints.exe
+
+res\DelphiTranslator.exe test\06_bool_expressions.pas test\compiled\06_bool_expressions.cs
+"C:\Program Files\MSBuild\14.0\Bin\csc.exe" \test\compiled\06_bool_expressions.cs /out:test\compiled\06_bool_expressions.exe
+\test\compiled\06_bool_expressions.exe
+
+res\DelphiTranslator.exe test\07_bool_operator_precedence.pas test\compiled\07_bool_operator_precedence.cs
+"C:\Program Files\MSBuild\14.0\Bin\csc.exe" \test\compiled\07_bool_operator_precedence.cs /out:test\compiled\07_bool_operator_precedence.exe
+\test\compiled\07_bool_operator_precedence.exe
+
+res\DelphiTranslator.exe test\08_bool_expressions_with_vars.pas test\compiled\08_bool_expressions_with_vars.cs
+"C:\Program Files\MSBuild\14.0\Bin\csc.exe" \test\compiled\08_bool_expressions_with_vars.cs /out:test\compiled\08_bool_expressions_with_vars.exe
+\test\compiled\08_bool_expressions_with_vars.exe
+
+res\DelphiTranslator.exe test\09_double_expressions.pas test\compiled\09_double_expressions.cs
+"C:\Program Files\MSBuild\14.0\Bin\csc.exe" \test\compiled\09_double_expressions.cs /out:test\compiled\09_double_expressions.exe
+\test\compiled\09_double_expressions.exe
+
+res\DelphiTranslator.exe test\10_double_operator_precedence.pas test\compiled\10_double_operator_precedence.cs
+"C:\Program Files\MSBuild\14.0\Bin\csc.exe" \test\compiled\10_double_operator_precedence.cs /out:test\compiled\10_double_operator_precedence.exe
+\test\compiled\10_double_operator_precedence.exe
+
+res\DelphiTranslator.exe test\11_double_expressions_with_vars.pas test\compiled\11_double_expressions_with_vars.cs
+"C:\Program Files\MSBuild\14.0\Bin\csc.exe" \test\compiled\11_double_expressions_with_vars.cs /out:test\compiled\11_double_expressions_with_vars.exe
+\test\compiled\11_double_expressions_with_vars.exe
+
+res\DelphiTranslator.exe test\12_while_loop.pas test\compiled\12_while_loop.cs
+"C:\Program Files\MSBuild\14.0\Bin\csc.exe" \test\compiled\12_while_loop.cs /out:test\compiled\12_while_loop.exe
+\test\compiled\12_while_loop.exe
+
+res\DelphiTranslator.exe test\13_while_loop_single_stmt.pas test\compiled\13_while_loop_single_stmt.cs
+"C:\Program Files\MSBuild\14.0\Bin\csc.exe" \test\compiled\13_while_loop_single_stmt.cs /out:test\compiled\13_while_loop_single_stmt.exe
+\test\compiled\13_while_loop_single_stmt.exe
+
+res\DelphiTranslator.exe test\14_if_stmt.pas test\compiled\14_if_stmt.cs
+"C:\Program Files\MSBuild\14.0\Bin\csc.exe" \test\compiled\14_if_stmt.cs /out:test\compiled\14_if_stmt.exe
+\test\compiled\14_if_stmt.exe
+
+res\DelphiTranslator.exe test\15_if_stmt_single_stmt.pas test\compiled\15_if_stmt_single_stmt.cs
+"C:\Program Files\MSBuild\14.0\Bin\csc.exe" \test\compiled\15_if_stmt_single_stmt.cs /out:test\compiled\15_if_stmt_single_stmt.exe
+\test\compiled\15_if_stmt_single_stmt.exe
+
+res\DelphiTranslator.exe test\16_nested_ifs.pas test\compiled\16_nested_ifs.cs
+"C:\Program Files\MSBuild\14.0\Bin\csc.exe" \test\compiled\16_nested_ifs.cs /out:test\compiled\16_nested_ifs.exe
+\test\compiled\16_nested_ifs.exe
+
+res\DelphiTranslator.exe test\17_long_variable_names.pas test\compiled\17_long_variable_names.cs
+"C:\Program Files\MSBuild\14.0\Bin\csc.exe" \test\compiled\17_long_variable_names.cs /out:test\compiled\17_long_variable_names.exe
+\test\compiled\17_long_variable_names.exe
+
